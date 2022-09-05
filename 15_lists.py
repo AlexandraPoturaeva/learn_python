6bc3728526349ece40f7743321dcfb86e338841f
AlexandraPoturaeva <av-poturaeva@yandex.ru>
2022-09-05 13:18:24 +0300

    Немного усовершенствовала их метод решения 10-й задачи - циклу не нужно теперь проверять одно значение - сравнивать себя с собой, теперь цикл сразу переходит к следующему числу.

--- 15_lists.py
+++ 15_lists.py
@@ -656,7 +656,7 @@ print('Изначальный список:', numbers_list)
 
 def selection_sort(my_list):
     for i_mn in range(len(my_list)):
-        for curr in range(i_mn, len(my_list)):
+        for curr in range(i_mn + 1, len(my_list)):
             if my_list[curr] < my_list[i_mn]:
                 my_list[curr], my_list[i_mn] = my_list[i_mn], my_list[curr]
 
