d39a79171f649903f3c17c2015e3781ff076b789
AlexandraPoturaeva <av-poturaeva@yandex.ru>
2022-10-10 12:37:14 +0300

    Сделаны задачи из урока 18.3 Методы строк: split и join

--- 18_base_collections_strings.py
+++ 18_base_collections_strings.py
@@ -2,6 +2,8 @@
 # Задача 1. Заказ
 import random
 
+from my_modules import helper
+
 names = ['Александр', 'Агата', 'Аделина', 'Адель', 'Айдар', 'Александра', 'Алексей', 'Алёна', 'Алина', 'Алиса', 'Алия',
          'Алла', 'Альберт', 'Альбина', 'Амалия', 'Амелия', 'Амина', 'Амир', 'Анастасия', 'Анатолий', 'Ангелина',
          'Андрей', 'Анжелика', 'Анна', 'Антон', 'Антонина', 'Ариана', 'Арина', 'Арсен', 'Арсений', 'Артём', 'Артемий',
@@ -50,3 +52,56 @@ names = ['Александр', 'Агата', 'Аделина', 'Адель', 'А
 
 # 18.3 Методы строк: split и join
 
+# Задача 1. Улучшенная лингвистика 2
+
+# words_to_find_list = helper.make_random_text_list('words', 3)
+# text_where_to_find = helper.make_random_text_list('words', 20)
+#
+# words_to_find_str = ' '.join(words_to_find_list)
+# text_where_to_find_str = ' '.join(text_where_to_find)
+#
+# print('Список слов для подсчёта их частоты употребления в тексте:', words_to_find_str, '\nТекст:', text_where_to_find_str)
+#
+# words_freq_dict = {i: text_where_to_find.count(i) for i in words_to_find_list}
+#
+# for key, value in words_freq_dict.items():
+#     print(key + ': ' + str(value))
+
+# Задача 2. Бабушка
+
+# text = ''
+#
+# for _ in range(random.randint(3, 7)):
+#     text += (helper.make_random_letters_sequence() + (' ' * random.randint(1, 4)))
+#
+# print('Текст:', text)
+#
+# text_list = text.split()
+#
+# correct_text = ' '.join(text_list)
+#
+# print(correct_text)
+
+# Задача 3. Разделители символов
+
+grats_template = 'С днём рождения, {name}! Тебе уже {age} лет!'
+
+persons_count = random.randint(1, 10)
+
+names_list = helper.make_random_text_list('names_male', persons_count)
+surnames_list = helper.make_random_text_list('russian_surnames', persons_count)
+
+birthday_pers_list = [' '.join([names_list[i], surnames_list[i]]) for i in range(persons_count)]
+birthday_pers_text = ', '.join(birthday_pers_list)
+print('Список людей через запятую:', birthday_pers_text)
+
+age_list = [str(random.randint(10, 100)) for i in range(persons_count)]
+age_text = ' '.join(age_list)
+print('Возраст людей через пробел:', age_text)
+
+for i_pers in range(persons_count):
+    print(grats_template.format(name = birthday_pers_list[i_pers], age = age_list[i_pers]))
+
+persons_and_age_list = [' '.join([birthday_pers_list[i], age_list[i]]) for i in range(persons_count)]
+persons_and_age_text = ', '.join(persons_and_age_list)
+print('\nИменинники:', persons_and_age_text)
