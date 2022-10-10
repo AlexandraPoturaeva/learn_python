e229609059664485401e222e8c178f0d22d442a7
AlexandraPoturaeva <av-poturaeva@yandex.ru>
2022-10-10 20:20:25 +0300

    Сделаны задачи из урока 18.4 Методы строк: startswith, endswith, upper, lower

--- 18_base_collections_strings.py
+++ 18_base_collections_strings.py
@@ -89,20 +89,20 @@ names = ['Александр', 'Агата', 'Аделина', 'Адель', 'А
 
 # Задача 3. Разделители символов
 
-grats_template = 'С днём рождения, {name}! Тебе уже {age} лет!'
-
-persons_count = random.randint(1, 10)
-
-names_list = helper.make_random_text_list('names_male', persons_count)
-surnames_list = helper.make_random_text_list('russian_surnames', persons_count)
-
-birthday_pers_list = [' '.join([names_list[i], surnames_list[i]]) for i in range(persons_count)]
-birthday_pers_text = ', '.join(birthday_pers_list)
-print('Список людей через запятую:', birthday_pers_text)
-
-age_list = [str(random.randint(10, 100)) for i in range(persons_count)]
-age_text = ' '.join(age_list)
-print('Возраст людей через пробел:', age_text)
+# grats_template = 'С днём рождения, {name}! Тебе уже {age} лет!'
+#
+# persons_count = random.randint(1, 10)
+#
+# names_list = helper.make_random_text_list('names_male', persons_count)
+# surnames_list = helper.make_random_text_list('russian_surnames', persons_count)
+#
+# birthday_pers_list = [' '.join([names_list[i], surnames_list[i]]) for i in range(persons_count)]
+# birthday_pers_text = ', '.join(birthday_pers_list)
+# print('Список людей через запятую:', birthday_pers_text)
+#
+# age_list = [str(random.randint(10, 100)) for i in range(persons_count)]
+# age_text = ' '.join(age_list)
+# print('Возраст людей через пробел:', age_text)
 
 # Метод 1
 # for i_pers in range(persons_count):
@@ -115,9 +115,87 @@ print('Возраст людей через пробел:', age_text)
 
 # Метод 3
 
-for age, name in zip(age_list, birthday_pers_list):
-    print(grats_template.format(name=name, age=age))
+# for age, name in zip(age_list, birthday_pers_list):
+#     print(grats_template.format(name=name, age=age))
+#
+# persons_and_age_list = [' '.join([birthday_pers_list[i], age_list[i]]) for i in range(persons_count)]
+# persons_and_age_text = ', '.join(persons_and_age_list)
+# print('\nИменинники:', persons_and_age_text)
 
-persons_and_age_list = [' '.join([birthday_pers_list[i], age_list[i]]) for i in range(persons_count)]
-persons_and_age_text = ', '.join(persons_and_age_list)
-print('\nИменинники:', persons_and_age_text)
+# 18.4 Методы строк: startswith, endswith, upper, lower
+
+# Задача 1. Шифр Цезаря 2
+
+# words_list = [helper.make_random_letters_sequence_up_low() for _ in range(random.randint(10, 30))]
+# text = ' '.join(words_list)
+# shift = random.randint(1, 26)
+# print('Сообщение:', text, '\nСдвиг:', shift)
+#
+# text_lowercase = text.lower()
+#
+# eng_alphabet = [chr(index) for index in range(ord("a"), ord("z") + 1)]
+#
+#
+# def caesar_cipher(user_text, user_shift):
+#     char_list = [(eng_alphabet[(eng_alphabet.index(sym) + user_shift) % 26] if sym != ' ' else ' ') for sym in user_text]
+#     return ''.join(char_list)
+#
+#
+# text_encrypted = caesar_cipher(text_lowercase, shift)
+# print('Зашифрованное сообщение:', text_encrypted)
+
+# Задача 2. Путь к файлу
+
+# paths_list = ['C:/user/docs/folder/new_file.txt', 'D:/user/docs/folder/new_file.txt',
+#               'C:/user/docs/folder/new_file.jpg', 'D:/user/docs/folder/new_file.jpg']
+#
+# path = random.choice(paths_list)
+# disk = random.choice(['C', 'D'])
+# file_extension = random.choice(['.txt', '.jpg'])
+#
+# print('Путь к файлу:', path,
+#       '\nНа каком диске должен лежать файл:', disk,
+#       '\nТребуемое расширение файла:', file_extension)
+#
+# if not path.startswith(disk):
+#     print('Ошибка. Неверно указан диск.')
+# elif not path.endswith(file_extension):
+#     print('Ошибка. Неверно указано расширение файла.')
+# else:
+#     print('Путь корректен!')
+
+# Задача 3. Удаление части
+
+# text = ' '.join([helper.make_random_letters_sequence_up_low() for _ in range(random.randint(1, 10))])
+# print('Текст:', text)
+
+# Метод 1 (мой)
+
+# low_count = 0
+# upper_count = 0
+
+# for sym in text:
+#     if sym.islower():
+#         low_count +=1
+#     elif sym.isupper():
+#         upper_count +=1
+
+# Метод 2 (из разбора дз)
+
+# low_count = len([letter for letter in text if letter.islower()])
+# upper_count = len([letter for letter in text if letter.isupper()])
+#
+# print('Количество заглавных букв:', upper_count,
+#       '\nКоличество строчных букв:', low_count,
+#       '\nРезультат: ', end='')
+#
+# if low_count > upper_count:
+#     print(text.lower())
+# elif low_count == upper_count:
+#     print('Количество заглавных и строчных букв одинаковое. Текст форматировать не нужно.')
+# else:
+#     print(text.upper())
+
+
+
+
