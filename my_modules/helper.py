8a569c94ea1802f3886901a6eb2ee2fd4df56dc9
AlexandraPoturaeva <av-poturaeva@yandex.ru>
2022-10-10 12:38:55 +0300

    В helper добавлена функция по созданию рандомного набора букв ("слов").

--- my_modules/helper.py
+++ my_modules/helper.py
@@ -1,5 +1,8 @@
 import pickle
+import string
+import random
 from random import randint
+
 
 def make_random_text_list(text_list_type, cnt):
     with open('./data/text_lists/' + text_list_type, 'rb') as f:
@@ -9,3 +12,12 @@ def make_random_text_list(text_list_type, cnt):
         word = text_list_for_random[randint(0, len(text_list_for_random) - 1)]
         random_text_list.append(word)
     return random_text_list
+
+
+def make_random_letters_sequence():
+    word = ''
+    letters_count = randint(1, 8)
+    for _ in range(letters_count):
+        letter = random.choice(string.ascii_lowercase)
+        word += letter
+    return word
